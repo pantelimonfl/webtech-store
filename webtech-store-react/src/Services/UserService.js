e2c90c924c51c55fb2c18533f01f0aea6c011f00
Florin Pantelimon <florinp@playtika.com>
2020-12-20 14:15:51 +0200

    - Am adaugat modulul UserService, unde pastram toate operatiunile ce tin de autentificarea utilizatorului: login, logout, isLoggedIn si getUserData.

---
+++ webtech-store-react/src/Services/UserService.js
@@ -0,0 +1,58 @@
+const jwt = require("jsonwebtoken");
+export const UserService = {
+  login,
+  logout,
+  getUserData,
+  isLoggedIn,
+};
+
+function login(email, password) {
+  const loginEndpoint = `http://localhost:8000/api/users/login?Email=${email}&Password=${password}`;
+  const requestOptions = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  return fetch(loginEndpoint, requestOptions)
+    .then(handleResponse)
+    .then((token) => {
+      if (token) {
+        localStorage.setItem("auth_token", token);
+      }
+
+      return token;
+    });
+}
+
+function isLoggedIn() {
+  return localStorage.getItem("auth_token");
+}
+
+function logout() {
+  localStorage.removeItem("auth_token");
+}
+
+function getUserData() {
+  const token = localStorage.getItem("auth_token");
+  if (token) {
+    const user = jwt.decode(token);
+    return {
+      email: user.Email,
+      name: user.Name,
+    };
+  }
+}
+
+function handleResponse(response) {
+  return response.text().then((text) => {
+    const data = text;
+    if (!response.ok) {
+      if (response.status === 401) {
+        logout();
+        window.location.reload();
+      }
+    }
+
+    return data;
+  });
+}
